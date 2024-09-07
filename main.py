0a67cdd44d5492f64b2b15ca5423cf32cfccd3eb
Senaphim <senaphim@gmail.com>
2024-09-07 12:26:43 +0100

    created inital black screen and got quit button working

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pygame
+from pygame.locals import *
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
