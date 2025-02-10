22ea6621a7faf8a02caa832699b6dc7498f356b4
Senaphim <senaphim@gmail.com>
2025-02-10 21:28:31 +0000

    added circle class and player class with circle class for hitbox

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from player import Player
 import os
 
 def main():
     pygame.init()
     
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x, y)
 
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        clock = pygame.time.Clock()
-        dt = 0
 
         screen.fill((0, 0, 0))
-        # pygame.display.update()
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
