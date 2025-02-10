377b11caf42165faa765ae02d660b35273de0d7b
Senaphim <senaphim@gmail.com>
2025-02-10 21:42:11 +0000

    made move method for player character

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -20,3 +20,23 @@ class Player(CircleShape):
     def draw(self, screen):
         triangle = self.triangle()
         pygame.draw.polygon(screen, (255, 255, 255), triangle, 2)
+        
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        forward *= PLAYER_SPEED * dt
+        self.position += forward
